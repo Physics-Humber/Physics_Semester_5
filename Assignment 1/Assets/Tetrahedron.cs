47b93f380b46774b0fd38eedff9807ac396d4449
CrescentLua <johnanoya.com@gmail.com>
2020-11-06 18:22:49 -0500

    GetFarthestPoint Function

--- Assignment 1/Assets/Tetrahedron.cs
+++ Assignment 1/Assets/Tetrahedron.cs
@@ -9,6 +9,17 @@ public class Tetrahedron : MonoBehaviour
 
 	public bool sharedVertices = false;
 
+	Vector3 A = new Vector3(0.5f, 0.8f, 0.3f);
+	Vector3 B = new Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 C = new Vector3(0.5f, 0.0f, 0.9f);
+	Vector3 D = new Vector3(1.0f, 0.0f, 0.0f);
+
+
+	Vector3 E = new Vector3(0.5f, 0.1f, 0.3f);
+	Vector3 F = new Vector3(0.0f, -0.7f, 0.0f);
+	Vector3 G = new Vector3(0.5f, -0.7f, 0.9f);
+	Vector3 H = new Vector3(1.0f, -0.7f, 0.0f);
+
 	Vector3 p0;
 	Vector3 p1;
 	Vector3 p2;
@@ -23,10 +34,25 @@ public class Tetrahedron : MonoBehaviour
 			return;
 		}
 
-		p0 = new Vector3(0, 0, 0);
-		p1 = new Vector3(1, 0, 0);
-		p2 = new Vector3(0.5f, 0, Mathf.Sqrt(0.75f));
-		p3 = new Vector3(0.5f, Mathf.Sqrt(0.75f), Mathf.Sqrt(0.75f) / 3);
+		
+	
+		if (gameObject.tag == "Tetra1")
+		{
+			p0 = new Vector3(A.x, A.y, A.z);
+			p1 = new Vector3(B.x, B.y, B.z);
+			p2 = new Vector3(C.x, C.y, C.z);
+			p3 = new Vector3(D.x, D.y, D.z);
+
+			Debug.Log("Tetra1: " + p0 + ", " + p1 + ", " + p2 + ", " + p3);
+		}
+
+		else if (gameObject.tag == "Tetra2")
+        {
+			p0 = new Vector3(E.x, E.y, E.z);
+			p1 = new Vector3(F.x, F.y, F.z);
+			p2 = new Vector3(G.x, G.y, G.z);
+			p3 = new Vector3(H.x, H.y, H.z);
+		}
 
 		Mesh mesh = meshFilter.sharedMesh;
 		if (mesh == null)
@@ -87,7 +113,68 @@ public class Tetrahedron : MonoBehaviour
 
 	public Vector3 getFarthestPointInDirection(Vector3 direction_)
     {
-		Vector3.Dot(p0, direction_);
+		Vector4 shape1DotProduct;
+		shape1DotProduct.x = Vector3.Dot(A, direction_);
+		shape1DotProduct.y = Vector3.Dot(B, direction_);
+		shape1DotProduct.z = Vector3.Dot(C, direction_);
+		shape1DotProduct.w = Vector3.Dot(D, direction_);
+
+		float maxDot = Mathf.Max(shape1DotProduct.x, shape1DotProduct.y, shape1DotProduct.z, shape1DotProduct.w);
+
+		Vector3 point1 = new Vector3(0.0f, 0.0f, 0.0f); 
+		if(maxDot.Equals(shape1DotProduct.x))
+        {
+			point1 = A; 
+        }
+
+		else if (maxDot.Equals(shape1DotProduct.y))
+        {
+			point1 = B;
+		}
+
+		else if (maxDot.Equals(shape1DotProduct.z))
+		{
+			point1 = C;
+		}
+
+		else if (maxDot.Equals(shape1DotProduct.w))
+		{
+			point1 = D;
+		}
+
+
+		Vector4 shape2DotProduct;
+		shape2DotProduct.x = Vector3.Dot(E, direction_);
+		shape2DotProduct.y = Vector3.Dot(F, direction_);
+		shape2DotProduct.z = Vector3.Dot(G, direction_);
+		shape2DotProduct.w = Vector3.Dot(H, direction_);
+
+		float minDot = Mathf.Min(shape2DotProduct.x, shape2DotProduct.y, shape2DotProduct.z, shape2DotProduct.w);
+
+		Vector3 point2 = new Vector3(0.0f, 0.0f, 0.0f);
+		if (minDot.Equals(shape2DotProduct.x))
+		{
+			point2 = E;
+		}
+
+		else if (minDot.Equals(shape2DotProduct.y))
+		{
+			point2 = F;
+		}
+
+		else if (minDot.Equals(shape2DotProduct.z))
+		{
+			point2 = G;
+		}
+
+		else if (minDot.Equals(shape2DotProduct.w))
+		{
+			point2 = H;
+		}
+
+		Vector3 newResult = point1 - point2;
+
+		return newResult;
     }
 
 	// Use this for initialization
@@ -99,6 +186,6 @@ public class Tetrahedron : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-
+		Rebuild();
 	}
 }
