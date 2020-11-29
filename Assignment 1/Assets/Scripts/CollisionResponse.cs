87c944dc0fd140b018e7701073242e49be6ddf96
CrescentLua <johnanoya.com@gmail.com>
2020-11-28 21:38:07 -0500

    Collision Response Implementation

--- Assignment 1/Assets/Scripts/CollisionResponse.cs
+++ Assignment 1/Assets/Scripts/CollisionResponse.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CollisionResponse : MonoBehaviour
 {
     Vector3 n = new Vector3(2.0f/3.0f, 1.0f/3.0f, 2.0f/3.0f);
+    Vector3 pointOfCollision = new Vector3(-8.0f, 1.0f, 0.0f); 
     float coefficient = 0.5f; 
 
     StarShip starShip;
     Asteroid asteroid;
-    Quaternion matrix;
+   
     
     void Start()
     {
@@ -18,18 +19,53 @@ public class CollisionResponse : MonoBehaviour
         asteroid.initialVelocity = new Vector3(20000.0f, 10000.0f, 20000.0f);
 
         starShip.mass = 100.0f;
-        asteroid.mass = 1.0f; 
+        asteroid.mass = 1.0f;
 
-        starShip.intertia = new Vector3(20.0f, 40.0f, 20.0f);
-        asteroid.intertia = new Vector3(0.1f, 0.1f, 0.1f);
+        starShip.centreMass = new Vector3(0.0f, 0.0f, 0.0f);
+        asteroid.centreMass = new Vector3(-10.0f, 2.0f, 0.0f);
+
+        starShip.intertia.SetRow(0, new Vector4(20.0f, 0.0f, 0.0f, 0.0f));
+        starShip.intertia.SetRow(1, new Vector4(0.0f, 40.0f, 0.0f, 0.0f));
+        starShip.intertia.SetRow(2, new Vector4(0.0f, 0.0f, 20.0f, 0.0f));
+        starShip.intertia.SetRow(3, new Vector4(0.0f, 0.0f, 0.0f, 20.0f));
+
+        asteroid.intertia.SetRow(0, new Vector4(0.1f, 0.0f, 0.0f, 0.0f));
+        asteroid.intertia.SetRow(1, new Vector4(0.0f, 0.1f, 0.0f, 0.0f));
+        asteroid.intertia.SetRow(2, new Vector4(0.0f, 0.0f, 0.1f, 0.0f));
+        asteroid.intertia.SetRow(3, new Vector4(0.0f, 0.0f, 0.0f, 0.1f));
 
         float Vr = Vector3.Dot(n, starShip.initialVelocity - asteroid.initialVelocity);
-        float JImpulse = -Vr * (coefficient + 1.0f) / (1.0f / asteroid.mass) + (1.0f / starShip.mass);
-        asteroid.intertia.len
-        float test = Vector3.Dot(n, Mathf.Pow(asteroid.intertia.length(), -1) * new Vector3());
-       
+        Vector3 r1 = pointOfCollision - starShip.centreMass;
+        Vector3 r2 = pointOfCollision - asteroid.centreMass;
+
+        float JImpulseTmp = -Vr * (coefficient + 1.0f);  
+        float tmp = Vector3.Dot(n,  starShip.intertia.inverse * (Vector3.Cross(Vector3.Cross(r1, n), r1)));
+        float tmp2 = Vector3.Dot(n, asteroid.intertia.inverse * (Vector3.Cross(Vector3.Cross(r2, n), r2)));
+
+        
+        float tmp3 = (1.0f / asteroid.mass) + (1.0f / starShip.mass) + (tmp + tmp2);
+        float JImpulse = JImpulseTmp / tmp3;
+
+        Debug.Log("JImpulse = " + JImpulse);
+
+        Vector3 starShipFinalVelocity;
+        starShipFinalVelocity.x = (starShip.initialVelocity.x + JImpulse) * (n.x / starShip.mass);
+        starShipFinalVelocity.y = (starShip.initialVelocity.y + JImpulse) * (n.y / starShip.mass);
+        starShipFinalVelocity.z = (starShip.initialVelocity.z + JImpulse) * (n.z / starShip.mass);
+
+        Debug.Log("Starship Final Velocity = " + starShipFinalVelocity);
+
+        Vector3 tempAngularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 starShipFinalAngularVelocity;
+
+        Vector3 rCrossProduct = Vector3.Cross(r1, JImpulse * n);
+        starShipFinalAngularVelocity.x = (tempAngularVelocity.x + starShip.intertia.inverse.m00) * rCrossProduct.x;
+        starShipFinalAngularVelocity.y = (tempAngularVelocity.y + starShip.intertia.inverse.m11) * rCrossProduct.y;
+        starShipFinalAngularVelocity.z = (tempAngularVelocity.z + starShip.intertia.inverse.m22) * rCrossProduct.z;
+
+        Debug.Log("Starship Final Angular Velocity = " + starShipFinalAngularVelocity);
     }
 
-    
+
 }
                     
--- Assignment 1/Assets/Scripts/Starship.cs
+++ Assignment 1/Assets/Scripts/Starship.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 struct StarShip
 {
-    public Vector3 initialVelocity;
     public float mass;
-    public Vector3 intertia;
+    public Vector3 initialVelocity;
+    public Vector3 centreMass; 
+    public Matrix4x4 intertia;
 }
